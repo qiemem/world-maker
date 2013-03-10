eeaaf4e4d435a69c490d198f3d289d46bde47044
Bryan Head <bryan.head@gmail.com>
2013-03-10 17:14:21 -0500

    Add number selection functionality.

--- editor/editor.js
+++ editor/editor.js
@@ -22,17 +22,32 @@ Editor = (function() {
     this.visible = false;
     this.evalTimeout = -1;
 
+    this.numberSelector = document.createElement("div");
+    this.numberSelector.id = ("number-selector");
+    container.appendChild(this.numberSelector);
+
+    this.ast;
+
     this.evalListeners = [];
-    var self = this;
+    var me = this;
     this.editor.on('change', function() {
-      clearTimeout(self.evalTimeout);
-      self.evalTimeout = setTimeout(function() {
-        self.evalContents();
+      clearTimeout(me.evalTimeout);
+      me.evalTimeout = setTimeout(function() {
+        me.ast = self.acorn.parse_dammit(me.editor.getValue());
+        me.evalContents();
       }, 100);
     });
-    var complete = function() {self.doCompletions();};
+
+    var complete = function() {me.doCompletions();};
     this.editor.on('cursorActivity', complete);
     this.editor.on('focus', complete);
+
+    this.editor.getScrollerElement().addEventListener("mousemove", function(e) {
+      var pos = {top: e.pageY, left: e.pageX};
+      var loc = me.editor.coordsChar(pos);
+      var token = me.editor.getTokenAt(loc);
+      me.mouseOnToken(loc, token);
+    });
   }
 
   Editor.prototype.evalContents = function() {
@@ -76,42 +91,91 @@ Editor = (function() {
       ,".cube()"
       , ".sphere()"
       , ".color('red')"
-      , ".rgb(.2, .5, .7)"
-      , ".hsl(.2, 1, .5)"
-      , ".forward(1)"
-      , ".backward(1)"
+      , ".rgb(0.20, 0.50, 0.70)"
+      , ".hsl(0.20, 1.0, 0.5)"
+      , ".forward(1.0)"
+      , ".backward(1.0)"
       , ".right(90)"
       , ".left(90)"
       , ".upward(90)"
       , ".downward(90)"
       , ".rollRight(90)"
       , ".rollLeft(90)"
-      , ".grow(1)"
-      , ".growWide(1)"
-      , ".growLong(1)"
-      , ".growTall(1)"
-      , ".transparency(.5)"];
+      , ".grow(1.0)"
+      , ".growWide(1.0)"
+      , ".growLong(1.0)"
+      , ".growTall(1.0)"
+      , ".transparency(0.5)"];
   }
 
   Editor.prototype.showCompletions = function(completions) {
     var li = d3.select(this.completionsList).selectAll("li").data(completions);
-    var self = this;
+    var me = this;
     li.enter().append("li")
       .text(function (d) {return d;})
       .classed("completion", 1)
       .on("mouseover", function(d) {
-        self.editor.replaceSelection(d);
+        me.editor.replaceSelection(d);
       })
       .on("mouseout", function(d) {
-        self.editor.replaceSelection("");
+        me.editor.replaceSelection("");
       })
       .on("click", function() {
-        self.editor.setCursor(self.editor.getCursor("end"));
-        self.editor.focus();
+        me.editor.setCursor(me.editor.getCursor("end"));
+        me.editor.focus();
       })
         ;
       
     li.exit().remove();
+  }
+
+  /**
+   * loc - {line, ch}
+   */
+  Editor.prototype.mouseOnToken = function(loc, token) {
+    switch (token.type) {
+      case "number":
+        // TODO: Make this tolerant of ".1"
+        this.numberSelector.style.display = "inline";
+        var startLoc = {line: loc.line, ch: token.start};
+        var endLoc = {line: loc.line, ch: token.end};
+        var startPos = this.editor.cursorCoords(startLoc);
+        var endPos = this.editor.cursorCoords(endLoc);
+
+        this.numberSelector.style.top = startPos.top + "px";
+        this.numberSelector.style.left = startPos.left + "px";
+
+        var string = token.string;
+        var decimal = token.string.indexOf(".");
+        var numDecimals = decimal < 0 ? 0 : string.length - decimal - 1;
+        var value = parseInt(token.string);
+        var me = this;
+        this.numberSelector.onmousedown = function(e) {
+          var lastVal = e.pageY;
+
+          function drag(e) {
+            var curVal = e.pageY;
+            var diff = Math.round((lastVal - curVal)/2);
+            var changeBy = numDecimals === 0 ? diff : diff / Math.pow(10, numDecimals);
+            var newStr = (value + changeBy).toFixed(numDecimals);
+            me.editor.replaceRange(newStr, startLoc, endLoc);
+            endLoc.ch = startLoc.ch + newStr.length;
+            me.evalContents();
+          }
+
+          window.addEventListener("mousemove", drag);
+          window.addEventListener("mouseup", stop);
+          function stop(e) {
+            window.removeEventListener("mousemove", drag);
+            window.removeEventListener("mouseup", stop);
+          }
+        };
+        break;
+      default:
+        this.numberSelector.style.display = "none";
+        this.numberSelector.onmousedown = null;
+        this.editor.focus();
+    }
   }
 
   return Editor;
