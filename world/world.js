b1cc159e853622da6bc3dda8d21010c3313fc3a4
Bryan Head <bryan.head@gmail.com>
2013-03-13 12:55:45 -0500

    Switch to revealing module pattern. Camera switch now fully working.

--- world/world.js
+++ world/world.js
@@ -9,34 +9,33 @@ World = (function () {
       camera,
       scene,
       controlModes,
-      controls;
+      controls,
+      activeControlMode,
+      playerLight;
 
-  World.ControlModes = {
+  var ControlModes = {
     FIRST_PERSON: "firstPerson",
     TRACKBALL:    "trackball"
   }
 
   var clock = new THREE.Clock();
 
-  World.init = function(container) {
+  var init = function(container) {
     renderer = new THREE.WebGLRenderer();
     
     var aspect = window.innerWidth/window.innerHeight;
     camera = new THREE.PerspectiveCamera(VIEW_ANGLE, aspect, NEAR, FAR);
 
     trackballControls = new THREE.TrackballControls(camera, renderer.domElement);
-    World.trackballControls = trackballControls
+    trackballControls = trackballControls
     trackballControls.rotateSpeed = 1.5;
     trackballControls.zoomSpeed = 12.0;
     trackballControls.panSpeed = 1.0;
     trackballControls.staticMoving = true;
     trackballPosition = camera.position.clone().setZ(20);
-    trackballRotation = camera.rotation.clone();
+    trackballUp = camera.up.clone();
     trackballControls.setActive = function(val) {
-      console.log(this.noRotate);
-      this.noRotate = val;
-      this.noZoom = val;
-      this.noPan = val;
+      this.enabled = val;
     };
       
     trackballControls.setActive(false);
@@ -45,52 +44,51 @@ World = (function () {
     fpControls.lookSpeed = .1;
     fpControls.movementSpeed = 2;
     fpPosition = camera.position.clone();
-    fpRotation = camera.rotation.clone();
+    fpUp = camera.up.clone();
     fpControls.setActive = function(val) {
       this.freeze = !val;
+      this.activeLook = val;
     };
     fpControls.setActive(false);;
 
+    controlList = [trackballControls, fpControls];
     controlModes = {
       trackball: {
         controls: trackballControls,
         position: trackballPosition,
-        rotation: trackballRotation
+        up: trackballUp
       }, 
       firstPerson: {
         controls: fpControls,
         position: fpPosition,
-        rotation: fpRotation
+        up: fpUp
       }
     }
 
-    controls = controlModes.firstPerson.controls;
-    controls.setActive(true);
-    camera.position.copy(controlModes.firstPerson.position);
-    camera.rotation.copy(controlModes.firstPerson.rotation);
+    switchControls(ControlModes.FIRST_PERSON);
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(new THREE.Color(0x000000, 1));
 
     THREEx.WindowResize(renderer, camera);
-    window.addEventListener('resize', function() {controls.handleResize();});
+    for (var i=0; i<controlList.length; i++) {
+      var c = controlList[i];
+      if (c.handleResize) {
+        window.addEventListener('resize', function() {c.handleResize();});
+      }
+    }
 
     scene = new THREE.Scene();
     scene.add(camera);
     
-    World.scene = scene;
-    World.camera = camera;
-    World.controls = controls;
-    World.renderer = renderer;
-
     container.appendChild(renderer.domElement);
 
-    World.reset();
+    reset();
 
-    World.animate();
+    animate();
   }
 
-  World.reset = function() {
+  var reset = function() {
     for (var i = scene.children.length - 1; i>=0; i--) {
       obj = scene.children[i];
       if (obj !== camera) {
@@ -102,26 +100,44 @@ World = (function () {
     scene.scale.set(1,1,1);
     scene.position.set(0,0,0);
 
-    //window.cursor = World.cursor().fd(5);
-
-    World.playerLight = new THREE.PointLight(0xFFFFFF);
-    scene.add(World.playerLight);
+    playerLight = new THREE.PointLight(0xFFFFFF);
+    scene.add(playerLight);
   }
 
-  World.switchControls = function(controlMode) {
-    controls.setActive(false);
-    var cm = controlModes[controlMode];
-    controls = cm.controls
-    camera.position.copy(cm.position);
-    camera.rotation.copy(cm.rotation);
+  var switchControls = function(controlMode) {
+    if (controls) {
+      controls.setActive(false);
+    }
+    if (activeControlMode) {
+      activeControlMode.position.copy(camera.position);
+      activeControlMode.up.copy(camera.up);
+    }
+    activeControlMode = controlModes[controlMode];
+    controls = activeControlMode.controls;
+    controls.setActive(true);      
+    camera.position.copy(activeControlMode.position);
+    camera.up.copy(activeControlMode.up);
+    camera.updateMatrix();
   }
 
-  World.animate = function() {
-    World.playerLight.position.copy(World.camera.position);
+  var animate = function() {
+    playerLight.position.copy(camera.position);
     controls.update(clock.getDelta());
     renderer.render(scene, camera);
-    requestAnimationFrame(World.animate);
+    requestAnimationFrame(animate);
   }
 
+  return {
+    ControlModes: ControlModes,
+    init: init,
+    switchControls: switchControls,
+    scene: function() {return scene},
+    camera: function() {return camera},
+    controls: function() {return controls},
+    renderer: function() {return renderer},
+    reset: reset,
+    playerLight: function() {return playerLight},
+    animate: animate
+  }
   return World;
 }());
