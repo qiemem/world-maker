ef22470c875ae71931ce2a1378e4a6ff4a5a15ce
Bryan Head <bryan.head@gmail.com>
2013-06-28 13:32:48 -0500

    Change "cursor" to "hand". Adjust its looks slightly.

--- levels/levels.js
+++ levels/levels.js
@@ -1,6 +1,6 @@
 var levels = (function() {
   'use strict';
-  
+
   /*
    * Needs:
    * - setup level
@@ -11,7 +11,8 @@ var levels = (function() {
    * - provide type definitions
    *   - in particular of objects available to user
    */
-  function Level (reEval, typedefs, typedefNames) {
+  function Level (initialContent, reEval, typedefs, typedefNames) {
+    this.initialContent = initialContent;
     this.reEval = reEval;
     this.typedefs = typedefs;
     this.typedefNames = typedefNames;
@@ -25,34 +26,39 @@ var levels = (function() {
     }
   }
 
-  var freeplay = new Level(function(code) {
-    if (!this.scene) {
-      this.scene = new agency.SceneAgent(World.scene(), World.renderer());
-      this.scene.onTick(function() {
-        requestAnimationFrame(this.notify.bind(this, 'tick'));
-      });
-      this.scene.notify('tick');
-    }
-    this.scene.killChildren();
-    new Function('scene', 'repeat', code) (this.scene, agency.repeat);
-  }, ({
-    basic: {
-      "!name": "basic",
-      // I should be able to do 
-      // repeat: "agency.repeat"
-      repeat: {
-        "!type": "fn(times: number, fn(iteration: number))",
-        "!suggest": "(10, function() {\n\n});\n"
-      },
-      // Both
-      // +agency.Agent
-      // and
-      // agency.Agent.prototype
-      // should work, but they don't. They work when I put the declarations
-      // in agency.json itself.
-      scene: "+agency.Agent"
-    }
-  }), ['agency/agency.json', 'basic']);
+  var freeplay = new Level( [
+      "var hand = new agency.HandAgent();",
+      "scene.addChild(hand);",
+      "// The above code gives you the hand to play with."
+    ].join('\n'),
+      function(code) {
+      if (!this.scene) {
+        this.scene = new agency.SceneAgent(World.scene(), World.renderer());
+        this.scene.onTick(function() {
+          requestAnimationFrame(this.notify.bind(this, 'tick'));
+        });
+        this.scene.notify('tick');
+      }
+      this.scene.killChildren();
+      new Function('scene', 'repeat', code) (this.scene, agency.repeat);
+    }, ({
+      basic: {
+        "!name": "basic",
+        // I should be able to do
+        // repeat: "agency.repeat"
+        repeat: {
+          "!type": "fn(times: number, fn(iteration: number))",
+          "!suggest": "(10, function() {\n\n});\n"
+        },
+        // Both
+        // +agency.Agent
+        // and
+        // agency.Agent.prototype
+        // should work, but they don't. They work when I put the declarations
+        // in agency.json itself.
+        scene: "+agency.Agent"
+      }
+    }), ['agency/agency.json', 'basic']);
 
   return {
     freeplay: freeplay
