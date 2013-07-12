f186ee035b44e8175ac2e86f8ca2ef82d4863d52
Bryan Head <bryan.head@gmail.com>
2013-07-12 00:59:20 -0500

    Add ramp agent

---
+++ codedrop/agents.js
@@ -0,0 +1,20 @@
+CodeDrope = window.CodeDrop || {};
+CodeDrope.agents = (function (agency) {
+  'use strict';
+
+  agency.Agent.prototype.ramp = function () {
+    return this.make(RampAgent);
+  };
+
+  function RampAgent () {
+    agency.CompositeAgent.call(this);
+    this.makeChild(agency.CubeAgent).growTall(-.75).rollRight(15).bounciness(0).friction(0);
+    this.makeChild(agency.CubeAgent).growTall(-.75).rollLeft(15).bounciness(0).friction(0);
+  }
+
+  RampAgent.prototype = Object.create(agency.CompositeAgent.prototype);
+  
+  return {
+    RampAgent: RampAgent
+  }
+})(agency)
