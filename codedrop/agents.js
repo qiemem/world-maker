f5b94ab052445e366daac1dfee613202a6d07edd
Bryan Head <bryan.head@gmail.com>
2013-07-12 02:57:10 -0500

    Add ball and generator

--- codedrop/agents.js
+++ codedrop/agents.js
@@ -8,13 +8,33 @@ CodeDrop.agents = (function (agency) {
 
   function RampAgent () {
     agency.CompositeAgent.call(this);
-    this.makeChild(agency.CubeAgent).growTall(-.75).rollRight(15).bounciness(0).friction(0);
-    this.makeChild(agency.CubeAgent).growTall(-.75).rollLeft(15).bounciness(0).friction(0);
+    this.makeChild(agency.CubeAgent).growTall(-.75).rollRight(45).bounciness(0).friction(0);
+    this.makeChild(agency.CubeAgent).growTall(-.75).rollLeft(45).bounciness(0).friction(0);
   }
 
   RampAgent.prototype = Object.create(agency.CompositeAgent.prototype);
+
+  function Ball () {
+    var sphere = new Physijs.SphereMesh(Ball.geometry, Ball.material, 1);
+    agency.Agent.call(this, sphere);
+    this.points = 1;
+    this.physicalFactor = 0.5;
+    this.__updatePhysical();
+  };
+  Ball.material = Physijs.createMaterial(new THREE.MeshPhongMaterial(), .5, .5);
+  Ball.geometry = new THREE.SphereGeometry(0.5, 16, 16);
+  Ball.prototype = Object.create(agency.Agent.prototype);
+
+  function Generator () {
+    agency.CubeAgent.call(this);
+    this.transparency(0.5).physical(false);
+    this.every(2000, function () {this.make(Ball);});
+  }
+  Generator.prototype = Object.create(agency.CubeAgent.prototype);
   
   return {
-    RampAgent: RampAgent
+    RampAgent: RampAgent,
+    Ball: Ball,
+    Generator: Generator
   }
 })(agency)
